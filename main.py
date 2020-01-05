51a21e40134762e18d9a472f12d4fefe62557977
lukec11 <me@lukec.me>
2020-01-04 19:38:49 -0500

    updated for slack
    updated to include RTM, threaded responses, and emote reaction through slack

--- main.py
+++ main.py
@@ -15,11 +15,23 @@ with open ("config.json") as f:
     keypass = config["keypass"] #pass to privkey
     log1 = config["log1"] #location of mc log files
     log2 = config["log2"]
+    slackToken = config["slackToken"]
+    slackChannel = config["slackChannel"]
+    slackChannelId = config["slackChannelId"]
 
-def slackResponse(message):
-    print(message)
+slack_client = slack.WebClient(token = slackToken)
+def slackResponse(message, ts):
+    slack_client.chat_postMessage(token = slackToken, as_user=False, channel=slackChannel, text=message, thread_ts=ts)
+    
+def slackEmote(color, ts):
+    if color == 'green':
+        slack_client.reactions_add(token=slackToken, channel=slackChannelId, name="heavy_check_mark", timestamp=ts)
+    elif color == 'white':
+        slack_client.reactions_add(token=slackToken, channel=slackChannelId, name="white_check_mark", timestamp=ts)
+    else:
+        print (f"Unknown color: color was reported as \"{color}\"")
 
-def modded(ign):
+def modded(ign, ts):
     #initialize ssh connection to server
     s = paramiko.SSHClient()
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -29,12 +41,12 @@ def modded(ign):
     s.exec_command(f"tmux send-keys -t 0 'whitelist add {ign}' Enter")
 
     #calls method to check whether or not the command worked
-    checksuccess(s, ign, log1)
+    checksuccess(s, ign, log1, ts, 'modded')
     
     #close ssh connection
     s.close()
 
-def vanilla(ign):
+def vanilla(ign, ts):
     #init ssh connection
     s = paramiko.SSHClient()
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -44,12 +56,12 @@ def vanilla(ign):
     s.exec_command(f"tmux send-keys -t 0 'whitelist add {ign}' Enter")
 
     #call method to check whether or not the command worked properly
-    checksuccess(s, ign, log2)
+    checksuccess(s, ign, log2, ts, 'vanilla')
 
     #close ssh connection
     s.close()
 
-def checksuccess(s, ign, log):
+def checksuccess(s, ign, log, ts, version):
     #signature
     signature = "Sign Server MODDED."
 
@@ -60,10 +72,32 @@ def checksuccess(s, ign, log):
     #decodes output of the command
     output = stdout.read().decode("utf-8")
     
-    #checks to ensure that the player was whitelisted
+    #checks to ensure that the player was whitelisted, and send response in slack
     if "to the whitelist" in output:
-        slackResponse(f"{ign} was added to the whitelist. {signature}")
+        slackResponse(f"{ign} was added to the whitelist. {signature}", ts)
+        if version == 'modded':
+            slackEmote('green', ts)
+        elif version == 'vanilla':
+            slackEmote('white', ts)
     else:
-        slackResponse(f'Error! Please check manually. Timestamp {datetime.now().strftime("%H:%M:%S")}. {signature}')
-        slackResponse(f"The latest line in the file was `{output}`.")
+        slackResponse(f'Error! Please check manually. Timestamp {datetime.now().strftime("%H:%M:%S")}. {signature}', ts)
+        slackResponse(f"The latest line in the file was `{output}`.", ts)
     
+@slack.RTMClient.run_on(event="message")
+def message_on(**payload):
+    ts = payload['data']['ts']
+    try:
+        data = payload['data']['text']
+        web_client = payload['web_client']
+
+        if data.startswith('!whitelist'):
+            modded(data[11:len(data)], ts)
+            #vanilla(data, ts)
+    except KeyError:
+        print ("threaded message, ignore.")
+
+
+#verification stuff for slack
+slack_token = slackToken
+rtm_client = slack.RTMClient(token=slack_token)
+rtm_client.start()
